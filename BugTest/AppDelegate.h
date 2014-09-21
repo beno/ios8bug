6c6000fa87940070c0dfcc71c9d62fb6a15f2f27
Michel Benevento <mbenevento@yahoo.com>
2014-09-21 10:22:34 +0200

    Initial Commit

---
+++ BugTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BugTest
+//
+//  Created by Michel Benevento on 21-09-14.
+//  Copyright (c) 2014 YAWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BugTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BugTest
+//
+//  Created by Michel Benevento on 21-09-14.
+//  Copyright (c) 2014 YAWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
